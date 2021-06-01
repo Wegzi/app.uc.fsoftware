bd0fe7ff7ce94af747b1398ed31d0a0d5a0e0609
Lucas <ramos.lucas@protonmail.com>
2021-05-31 23:20:02 -0300

    teste

--- serve.js
+++ serve.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log(`App is live on port ${3000}!`));
