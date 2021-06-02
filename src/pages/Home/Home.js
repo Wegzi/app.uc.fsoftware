fc996bd8931853fbad28d18d748a49c8f3859b6d
Lucas <ramos.lucas@protonmail.com>
2021-06-01 21:37:27 -0300

    fix warning

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -56,27 +56,27 @@ const SearchContainer = styled.div`
     font-size: 1rem;
   } */
 `;
-function Card() {
-  return (
-    <CardService className='rounded-lg p-3'>
-      <CardImage className='p-3 rounded-full mx-auto'></CardImage>
-      <div>
-        <p className='overflow-ellipsis overflow-hidden mt-3'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-          placerat, massa condimentum lacinia fringilla, ex mauris lacinia
-          tellus
-        </p>
-      </div>
-    </CardService>
-  );
-}
+// function Card() {
+//   return (
+//     <CardService className='rounded-lg p-3'>
+//       <CardImage className='p-3 rounded-full mx-auto'></CardImage>
+//       <div>
+//         <p className='overflow-ellipsis overflow-hidden mt-3'>
+//           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+//           placerat, massa condimentum lacinia fringilla, ex mauris lacinia
+//           tellus
+//         </p>
+//       </div>
+//     </CardService>
+//   );
+// }
 
-const CardImage = styled.div`
-  width: 70%;
-  height: 120px;
-  background-color: ${props => props.theme.primary};
-`;
-const CardService = styled.div`
-  min-height: 390px;
-  background-color: ${props => props.theme.light};
-`;
+// const CardImage = styled.div`
+//   width: 70%;
+//   height: 120px;
+//   background-color: ${props => props.theme.primary};
+// `;
+// const CardService = styled.div`
+//   min-height: 390px;
+//   background-color: ${props => props.theme.light};
+// `;
